f94a3a30dd58ccef6fce899b08baa1490fa7842b
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-09-19 10:39:47 -0700

    add readline

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,11 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -7,7 +14,12 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  rl.question("What is your name?", (name) => {
+    console.log(name);
+    rl.close();
+  });
+}
 
 // Function call to initialize app
 init();
