d3d5063ffadf866f9d45efba87f0148562f9c6f1
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-09-19 19:24:57 -0700

    add prompts npm package and remove prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,87 +1,79 @@
 // TODO: Include packages needed for this application
 const fs = require("fs");
-const prompt = require("prompt");
+const prompts = require("prompts");
+
 const generateMarkdown = require("./utils/generateMarkdown");
 
-prompt.start();
-
-// TODO: Create an array of questions for user input
-
-const questions = {
-  properties: {
-    title: {
-      description: "Title of your Project?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Title must be only letters, spaces, or dashes",
-      require: true,
-    },
-    description: {
-      description: "Describe your project?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Description must be only letters, spaces, or dashes",
-      require: true,
-    },
-    installation: {
-      description: "describe your project installation?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Content must be only letters, spaces, or dashes",
-      require: true,
-    },
-    usage: {
-      description: "Describe your project usage?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Content must be only letters, spaces, or dashes",
-      require: true,
-    },
-    license: {
-      description: "Describe your project License?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Content must be only letters, spaces, or dashes",
-      require: true,
-    },
-    contributing: {
-      description: "Describe your project Contributing?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Content must be only letters, spaces, or dashes",
-      require: true,
-    },
-    tests: {
-      description: "describe your project Tests?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Content must be only letters, spaces, or dashes",
-      require: true,
-    },
-    questions: {
-      description: "describe your project Questions?",
-      pattern: /^[a-zA-Z\s\-]+$/,
-      message: "Content must be only letters, spaces, or dashes",
-      require: true,
-    },
+//Create an array of questions for user input
+const questions = [
+  {
+    type: "text",
+    name: "title",
+    message: `Title of your Project?`,
   },
-};
-
+  {
+    type: "text",
+    name: "description",
+    message: `Describe your project?`,
+  },
+  {
+    type: "text",
+    name: "installation",
+    message: `describe your project installation?`,
+  },
+  {
+    type: "text",
+    name: "usage",
+    message: `Describe your project usage?`,
+  },
+  {
+    type: "text",
+    name: "contributing",
+    message: `Describe your project Contributing?`,
+  },
+  {
+    type: "text",
+    name: "tests",
+    message: `describe your project Tests?`,
+  },
+  {
+    type: "text",
+    name: "questions",
+    message: `describe your project Questions?`,
+  },
+  {
+    type: "select",
+    name: "license",
+    message: "Select a license for your project?",
+    choices: [
+      { title: "MIT", value: "mit" },
+      { title: "GNU", value: "gnu" },
+      { title: "Apache v2", value: "apache" },
+    ],
+    initial: 1,
+  },
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {
-  let fileContent = "";
-  //Ask for the Project Title
-  prompt.get(questions, function (err, result) {
-    console.log("============Results======================");
-    console.log(result);
-    //Create the  file Markdown
-    fileContent = generateMarkdown(result);
-
-    console.log(fileContent);
-    const fileName = `README-${result.title}.md`;
-    fs.writeFile(fileName, fileContent, (err) => {
-      if (err) throw err;
-      console.log(
-        `file README for your project ${result.title} was generated succesfully.`
-      );
-    });
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) throw err;
+    console.log(
+      `file README for your project ${data.title} was generated succesfully.`
+    );
   });
+}
+
+//Create a function to initialize app
+function init() {
+  let data = "";
+  //Ask for the Project Title
+  (async () => {
+    const response = await prompts(questions);
+    console.log(response);
+    data = generateMarkdown(response);
+    const fileName = `README-${response.title}.md`;
+    writeToFile(fileName, data);
+  })();
 }
 
 // Function call to initialize app
